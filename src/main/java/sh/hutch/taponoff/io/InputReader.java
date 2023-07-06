107aa3a80c20d5913afa11b34b1c9de76bdd1013
jdhutchison <github@hutch.sh>
2023-07-06 15:10:55 +1000

    Committing missed changes to io files.

--- src/main/java/sh/hutch/taponoff/io/InputReader.java
+++ src/main/java/sh/hutch/taponoff/io/InputReader.java
@@ -1,2 +1,60 @@
-package sh.hutch.taponoff.io;public class InputReader {
+package sh.hutch.taponoff.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Responsible for reading the input CSV */
+public class InputReader {
+  private static final String HEADER_ROW_START = "ID,";
+  private String filepath;
+
+  public InputReader(String filepath) {
+    this.filepath = filepath;
+    validateFilepath();
+  }
+
+  /**
+   * Checks we can read from the file we're meant to.
+   */
+  private void validateFilepath() {
+    if (filepath == null || filepath.isEmpty()) {
+      throw new IllegalArgumentException("Filepath cannot be null or empty");
+    }
+    File f = new File(filepath);
+    if (!f.exists()) {
+      System.err.printf("FATAL ERROR: File does not exist: %s%n", filepath);
+      throw new IllegalArgumentException("File does not exist: " + filepath);
+    } else if (!f.canRead()) {
+      System.err.printf("FATAL ERROR: File is not readable: %s%n", filepath);
+      throw new IllegalArgumentException("File is not readable: " + filepath);
+    } else if (!f.isFile()) {
+      System.err.printf("FATAL ERROR: Input file is not a file: %s%n", filepath);
+      throw new IllegalArgumentException("File is not a file: " + filepath);
+    }
+  }
+
+  /**
+   * Reads the input file and returns the lines as a list of strings.
+   * @return the lines from the input file
+   * @throws IOException if there is a problem reading the file
+   */
+  public List<String> read() throws IOException {
+    List<String> lines = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        if (!line.startsWith(HEADER_ROW_START)) {
+          lines.add(line);
+        }
+      }
+    } catch (IOException e) {
+      System.err.printf("Unable to read input file \"%s\": %s%n", filepath, e.getMessage());
+      throw e;
+    }
+    return lines;
+  }
 }
--- src/main/java/sh/hutch/taponoff/io/OutputWriter.java
+++ src/main/java/sh/hutch/taponoff/io/OutputWriter.java
@@ -1,2 +1,64 @@
-package sh.hutch.taponoff.io;public class OutputWriter {
+package sh.hutch.taponoff.io;
+
+import sh.hutch.taponoff.TapEventParser;
+import sh.hutch.taponoff.TripData;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Responsible for writing the output CSV, formatting the trips in the process.
+ */
+public class OutputWriter {
+
+  private static final String OUTPUT_HEADER =
+      "Started, Finished, DurationSecs, FromStopId, ToStopId, ChargeAmount, CompanyId, BusID, PAN, Status";
+  private String filepath;
+
+  public OutputWriter(String filepath) {
+    this.filepath = filepath;
+    validateFilepath();
+  }
+
+  /**
+   * Checks we can write to the file we're meant to.
+   */
+  private void validateFilepath() {
+    if (filepath == null || filepath.isEmpty()) {
+      throw new IllegalArgumentException("Filepath cannot be null or empty");
+    }
+    File f = new File(filepath);
+    if (f.exists()) {
+      System.err.printf("FATAL ERROR: File already exist: %s. Will not overwrite. %n", filepath);
+      throw new IllegalArgumentException("File already exist: " + filepath);
+    }
+  }
+
+  /**
+   * Writes the output file.
+   * @param trips the lines to write
+   */
+  public void write(List<TripData> trips) throws Exception {
+    try (PrintWriter writer = new PrintWriter(filepath)) {
+      writer.println(OUTPUT_HEADER);
+      for (TripData trip : trips) {
+        String fromTimestamp = trip.startTime().format(TapEventParser.FORMATTER);
+        String toTimestamp = "";
+        if (trip.endTime() != null) {
+          toTimestamp = trip.endTime().format(TapEventParser.FORMATTER);
+        }
+        String toStop = trip.endStopId() == null ? "" : trip.endStopId();
+        String duration = trip.durationInSeconds() == null ? "" : trip.durationInSeconds().toString();
+
+        String fare = String.format("$%.2f", trip.fare());
+        String line = String.format("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s", fromTimestamp, toTimestamp,
+            duration, trip.startStopId(), toStop, fare, trip.companyId(), trip.busId(), trip.pan(), trip.type());
+        writer.println(line);
+      }
+    } catch (Exception e) {
+      System.err.printf("OUTPUT ERROR: Unable to write output file \"%s\": %s%n", filepath, e.getMessage());
+      throw e;
+    }
+  }
 }
